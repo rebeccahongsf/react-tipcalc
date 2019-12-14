9c878a15cd3ba8a0d7d6b0c560821ca36d01b6e4
Rebecca Hong <Rebeccahongsf@gmail.com>
2019-12-14 11:57:12 -0800

    Test Hello.js

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Hello from './Hello';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
+      <Hello />
     </View>
   );
 }
---
+++ Hello.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Hello() {
+    return (
+        <View>
+            <Text>Hello from Hello.js!</Text>
+        </View>
+    );
+}
