e8bacea48ac446d392147a65032421562a43e79c
Rebecca Hong <Rebeccahongsf@gmail.com>
2019-12-14 14:40:37 -0800

    Test StyleSheet

--- Hello.js
+++ Hello.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function Hello() {
     return (
         <View>
-            <Text>Hello from Hello.js!</Text>
+            <Text style={styles.hello}>Hello from Hello.js!</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    hello: {
+        color: 'rebeccapurple',
+    }
+});
