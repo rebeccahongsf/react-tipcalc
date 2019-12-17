b97ced9d79c6dad03400ddbecc477d3cdc0df980
Rebecca Hong <Rebeccahongsf@gmail.com>
2019-12-16 20:25:27 -0800

    Add Tip Button

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import {  Button, 
+          StyleSheet, 
+          Text, 
+          View, 
+          TextInput 
+        } from 'react-native';
 
 import Hello from './Hello';
 
@@ -7,17 +12,47 @@ export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      inputValue: 'Hi',
+      inputValue: '',
     }
   };
 
   render() {
+    let tip = 0.00;
+    if(this.state.inputValue) {
+      tip = parseFloat(this.state.inputValue * this.state.tip)
+      tip = (Math.round(tip * 100)/100).toFixed(2);
+    }
     return (
       <View style={styles.container}>
+        <Text>
+          {tip} 
+          {this.state.tip}
+        </Text>
         <TextInput 
           value={this.state.inputValue}
-          style={styles.input} 
+          style={styles.input}
+          keyboardType='numeric'
+          placeholder='0.00'
+          onChangeText={(text) => this.setState({inputValue: text})}
         />
+        <View>
+          <Button
+            title="10%"
+            onPress={() => this.setState({tip : 0.1})}
+          />
+          <Button
+            title="15%"
+            onPress={() => this.setState({tip : 0.15})}
+          />
+          <Button
+            title="18%"
+            onPress={() => this.setState({tip : 0.18})}
+          />
+          <Button
+            title="20%"
+            onPress={() => this.setState({tip : 0.2})}
+          />
+        </View>
       </View>
     );
   }
